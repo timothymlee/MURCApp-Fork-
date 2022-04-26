40ee4e6e86eb3d947b5f780e0c3deb6c89a2194f
ReidBurger <rb1504@messiah.edu>
2022-04-26 19:24:15 -0400

    Update App for Demo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing the addition!</Text>
+      <Text>This is for the demo!</Text>
       <StatusBar style="auto" />
     </View>
   );
