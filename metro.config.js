da3be2aa3406c57e2e02853be7dc608c157c35a2
timothymlee <tl1261@messiah.edu>
2022-09-22 15:15:28 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,11 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+config.resolver.assetExts.push(
+  // Adds support for `.db` files for SQLite databases
+  'db'
+);
+
 defaultConfig.resolver.assetExts.push("cjs");
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = defaultConfig;
