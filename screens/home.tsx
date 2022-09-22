8d1a93d76bcbd2b89bcaf55eabd7d9f918d4a377
ReidBurger <rb1504@messiah.edu>
2022-09-21 20:56:43 -0400

    Added to Home Screen

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,5 +1,7 @@
 import { ImageBackground, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import React from 'react';
+import { Header, Icon, SearchBar } from "@rneui/themed";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 type CompProps = {
   // We are only using the navigate and goBack functions
@@ -7,31 +9,47 @@ type CompProps = {
 };
 
 export default function Home(props: CompProps) {
+  const [value, setValue] = React.useState("");
+
   return (
     <>
-        <View style={styles.container}>
-          <View style={[styles.something, styles.something]}>
-            <Text style={styles.something}>Unitivity Login</Text>
-            <TextInput
-            placeholderTextColor="#000" 
-            style={styles.something}
-            placeholder="EMAIL"
+        <View style={styles.page}>
+          <Header
+            backgroundImageStyle={{}}
+            barStyle="default"
+            centerComponent={{
+              text: "MU",
+              style: { color: "#fff", fontSize: 30 }
+            }}
+            centerContainerStyle={{}}
+            containerStyle={styles.header}
+            leftComponent={{ icon: "person", color: "#fff", size: 40 }}
+            leftContainerStyle={{}}
+            linearGradientProps={{}}
+            placement="center"
+            rightComponent={{ icon: "home", color: "#fff", size: 40 }}
+            rightContainerStyle={{}}
+            statusBarProps={{}}
+          />
+
+          <View style={styles.app_container}>
+          
+          </View>
+          
+          <View style={styles.search_container}>
+            <SearchBar
+              platform="default"
+              containerStyle={{backgroundColor: null}}
+              inputContainerStyle={{backgroundColor: '#F3F3F3', margin: 10}}
+              inputStyle={{}}
+              leftIconContainerStyle={{}}
+              rightIconContainerStyle={{}}
+              loadingProps={{}}
+              onChangeText={newVal => setValue(newVal)}
+              placeholder="Search"
+              placeholderTextColor="#888"
+              value={value}
             />
-            <TextInput
-            secureTextEntry={true}
-            placeholderTextColor="#000" 
-            style={styles.something}
-            placeholder="PASSWORD"
-            />
-            <Pressable  onPress={()=>{props.navigation.navigate('ForgotPassword')}}>
-              <Text style={styles.something}>Forgot Your Password?</Text>
-            </Pressable>
-            <Pressable style={styles.something} onPress={()=>{props.navigation.navigate('Explore')}}>
-              <Text style={styles.something}>Login</Text>
-            </Pressable>
-            <Pressable style={styles.something} onPress={()=>{ props.navigation.navigate('SignUp')}}>
-              <Text style={styles.something}>Sign Up</Text>
-            </Pressable>
           </View>
         </View>
     </>
@@ -39,10 +57,20 @@ export default function Home(props: CompProps) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  app_container: {
+    backgroundColor: '#CCC',
+    flex: 7
   },
-  something: {}
+  header: {
+    backgroundColor: '#1E293B',
+    flex: 1
+  },
+  search_container: {
+    backgroundColor: '#1E293B',
+    flex: 1.5
+  },
+  page: {
+    backgroundColor: 'red',
+    flex: 1
+  }
 });
