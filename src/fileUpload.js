9de2cea07015df5988cea4ce9ecc13656b1ea003
tl1261 <tl1261@messiah.edu>
2022-11-10 21:24:59 -0500

    Add screen + logic for uploading

---
+++ src/fileUpload.js
@@ -0,0 +1,50 @@
+//https://stackoverflow.com/questions/66967900/expo-document-picker-is-not-displaying-to-upload-a-file
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button,
+    TouchableOpacity,
+} from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+
+const UploadFile = () => {
+    const pickDocument = async () => {
+        let result = await DocumentPicker.getDocumentAsync({});
+        console.log(result.uri);
+        console.log(result);
+    };
+
+    return (
+        <View style={styles.background}>
+            <Text style={styles.file}>Upload Poster</Text>
+            <View style={styles.button}>
+                <TouchableOpacity>
+                    <Button
+                        title="upload your file"
+                        color="black"
+                        onPress={pickDocument}
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor:
+            "radial-gradient(ellipse at left bottom,    rgb(163, 237, 255) 0%,    rgba(57, 232, 255, 0.9) 59%,    rgba(48, 223, 214, 0.9) 100% )",
+    },
+    file: {
+        color: "black",
+        marginHorizontal: 145,
+    },
+    button: {
+        marginHorizontal: 60,
+    },
+});
+
+export default UploadFile;
