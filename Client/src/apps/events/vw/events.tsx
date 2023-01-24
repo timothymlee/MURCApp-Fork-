8a9fa13b04dafaaac2a62f7e2f08007d9aba4985
tl1261 <tl1261@messiah.edu>
2023-01-24 18:32:42 -0500

    Restore file changes with previous major updates

--- Client/src/apps/events/vw/events.tsx
+++ Client/src/apps/events/vw/events.tsx
@@ -6,6 +6,7 @@ import AnimatedInput from "react-native-animated-input";
 import { color, renderNode } from "@rneui/base";
 import {readEventData} from '../../../firebaseCalls';
 import { CalendarProvider, WeekCalendar } from 'react-native-calendars';
+import Header from "../../../utils/components/header";
 
 
 const todaysDate = ((new Date().getFullYear().toLocaleString()) + '-' + (new Date().getMonth() + 1).toLocaleString()) + '-' + (new Date().getDate().toLocaleString())
